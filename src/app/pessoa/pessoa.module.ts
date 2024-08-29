3d33e0bd987080244249c5fe01ed22b14191003a
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-08-28 23:34:58 -0300

    Created pipe caixa-alta

--- src/app/pessoa/pessoa.module.ts
+++ src/app/pessoa/pessoa.module.ts
@@ -9,6 +9,7 @@ import { EditarPessoaComponent } from './editar-pessoa/editar-pessoa.component';
 import { NumericoDirective } from '../shared/directives/numerico.directive';
 import { MinimoValidatorDirective } from '../shared/directives/minimo-validator.directive';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { CaixaAltaPipe } from '../shared/pipes/caixa-alta.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
     InserirPessoaComponent,
     EditarPessoaComponent,
     NumericoDirective,
-    MinimoValidatorDirective
+    MinimoValidatorDirective,
+    CaixaAltaPipe
   ],
   imports: [
     CommonModule,
