59648cf982860af76c2baedc18e264fcb6d8e3b5
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-08-31 08:52:25 -0300

    Modify model "pessoa" to the field "Idade" starts with 0 instead of unassigned

--- src/app/shared/models/pessoa.model.ts
+++ src/app/shared/models/pessoa.model.ts
@@ -2,7 +2,7 @@ export class Pessoa {
     constructor(
         public id?: number,
         public nome?: string,
-        public idade?: number,
+        public idade: number = 0,
         public dataNascimento? : string,
         public motorista?: string){
     }
